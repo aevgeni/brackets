fa8921dfefefca2460d1ac5a0e4198c25901da9d
aevgeni <eslilolko@mail.ru>
2019-03-22 13:01:05 +0300

    feat: task complete

--- src/index.js
+++ src/index.js
@@ -1,36 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-  let arrayString = str.split('');  
-  for (let i = 0; i <= bracketsConfig.length; i++) {
+  let arrayStr = str.split('');  
+  for (let i = 0; i <= 1; i++) {
     for (let k = 0; k < bracketsConfig.length; k++) {
-      console.log(k);
-        for (let p = 0; p < arrayString.length; p++) {
-          if (arrayString[p] == bracketsConfig[k][0] && arrayString[p+1] == bracketsConfig[k][1]) {
-            delete arrayString[p];   
-            delete arrayString[p+1]; 
-            arrayString = arrayString.filter(element => element !== undefined);
-            p = 0;
-            k = 0;
-          }
+      for (let l = 0; l < arrayStr.length; l++) {
+        if (arrayStr[l] == bracketsConfig[k][0] && arrayStr[l+1] == bracketsConfig[k][1]) {
+          delete arrayStr[l];
+          delete arrayStr[l+1];
+          arrayStr = arrayStr.filter(element => element !== undefined);
+          i = 0;
         }
-        
-            
+      }
     }
   }
-  if (arrayString.length == 0) {
+  if (arrayStr.length == 0) {
     return true;
   } else {
-  return false;}   
+  return false;
+  }   
 }
-
-
-const config1 = [['(', ')']];
-const config2 = [['(', ')'], ['[', ']']];
-const config3 = [['(', ')'], ['[', ']'], ['{', '}']];
-const config4 = [['|', '|']];
-const config5 = [['(', ')'], ['|', '|']];
-const config6 = [['1', '2'], ['3', '4'], ['5', '6'], ['7', '7'], ['8', '8']];
-const config7 = [['(', ')'], ['[', ']'], ['{', '}'], ['|', '|']];
-
-
-check('111115611111111222288888822225577877778775555666677777777776622222', config6);
